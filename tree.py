6398c84a0b41cde8bf2b88b17bac7068912352fc
Alexander Safonov <safonov@arrival.com>
2023-07-27 17:58:11 +0400

    Test fixture for Nodes is started to be written.

--- tree.py
+++ tree.py
@@ -12,7 +12,7 @@ class Node:
         self._content = None
 
 
-    def connect_to(self, parent : Node):
+    def connect_to(self, parent: Node):
         if self._parent is Node:
             self._parent.successors.remove(self)
         self._parent = parent
@@ -24,10 +24,10 @@ class Node:
         self._parent.successors.remove(self)
         self._parent = None
 
-    def replace_with(self, node : Node):
+    def replace_with(self, node: Node):
         cur_parent = self._parent
         if cur_parent is Node:
-            parent_descs = cur_parent._successors
+            parent_descs = cur_parent.successors
             cur_index = parent_descs.index(self)
             parent_descs[cur_index] = node
 
@@ -36,12 +36,12 @@ class Node:
 
 
     def is_root(self):
-        return self._parent == None
+        return self._parent is None
 
     def is_parent(self):
         return len(self._successors) > 0
 
-    def is_ancestor(self, second : Node):
+    def is_ancestor(self, second: Node):
         cur_parent = second._parent
         while cur_parent is not None:
             if cur_parent == self:
@@ -55,7 +55,7 @@ class Node:
 
         if self._parent is None:
             return
-        self._parent._successors.remove(self)
+        self._parent.successors.remove(self)
 
 
     @property
@@ -84,7 +84,7 @@ class Forest:
     # Public interface
     class NotNode(Exception): pass
     class NotLeaf(Exception): pass
-    class NotSuccessor(Exception) :pass
+    class NotSuccessor(Exception):pass
     class CyclingMovement(Exception): pass
     class AlienNode(Exception): pass
 
@@ -99,79 +99,100 @@ class Forest:
         self._roots.append(self._create_node())
         return self._roots[-1]
 
-    def add_leaf(self, parent : ForestNode) -> ForestNode:
+    def add_leaf(self, parent: ForestNode) -> ForestNode:
         self._validate_nodes(parent)
         leaf = self._create_node()
         leaf.connect_to(parent)
         return leaf
 
-    def insert_node_before(self, successor : ForestNode) -> ForestNode:
+    def insert_node_before(self, successor: ForestNode) -> ForestNode:
         self._validate_nodes(successor)
         new_node = self._create_node()
         successor.replace_with(new_node)
         successor.connect_to(new_node)
         return new_node
 
-    def insert_node_after(self, parent : ForestNode) -> ForestNode:
+    def insert_node_after(self, parent: ForestNode) -> ForestNode:
         self._validate_nodes(parent)
 
         new_node = self._create_node()
         new_node.connect_to(parent)
-        parent_descs = parent._successors
+        parent_descs = parent.successors
         for desc in parent_descs:
             desc.connect_to(new_node)
 
         return new_node
 
-    def move_node(self, node : ForestNode, new_parent : ForestNode):
+    def move_node(self, node: ForestNode, new_parent: ForestNode):
         self._validate_nodes(node, new_parent)
-        node_parent = node._parent
-        for desc in node._successors:
+        node_parent = node.parent
+        for desc in node.successors:
             desc.connect_to(node_parent)
         node.connect_to(new_parent)
 
-    def move_subtree(self, subroot : ForestNode, new_parent : ForestNode):
+    def move_subtree(self, subroot: ForestNode, new_parent: ForestNode):
         self._validate_nodes(subroot, new_parent)
         if subroot.is_ancestor(new_parent):
             raise Forest.CyclingMovement
         subroot.connect_to(new_parent)
 
-    def free_node(self, node : ForestNode):
+    def free_node(self, node: ForestNode):
         self._validate_nodes(node)
-        node_parent = node._parent
-        for desc in node._successors:
+        node_parent = node.parent
+        for desc in node.successors:
             desc.connect_to(node_parent)
         self._make_root(node)
 
-    def free_subtree(self, subroot : ForestNode):
+    def free_subtree(self, subroot: ForestNode):
         self._validate_nodes(subroot)
         self._make_root(subroot)
 
-    def delete_leafage(self, parent : ForestNode):
+    def delete_leafage(self, parent: ForestNode):
         self._validate_nodes(parent)
-        for desc in parent._successors:
+        for desc in parent.successors:
+            # TODO : Solve warning
+            # class A:
+            #     def __init__(self):
+            #         pass
+            #
+            #     def __del__(self):
+            #         print("Deleted")
+            #
+            # list_a = [A(), A(), A()]
+            # another_list_a = [A(), A(), A()]
+            # list_a.clear()
+            #
+            # output:
+            # Deleted
+            # Deleted
+            # Deleted
+            # Deleted
+            # Deleted
+            # Deleted
+            #
+            # Why six times?
             del desc
 
-    def delete_leaf(self, leaf : ForestNode):
+    def delete_leaf(self, leaf: ForestNode):
         self._validate_nodes(leaf)
         if leaf.is_parent():
             raise Forest.NotLeaf
         del leaf
 
-    def cut_node(self, node : ForestNode):
+    def cut_node(self, node: ForestNode):
         self._validate_nodes(node)
         if node.is_root():
             raise Forest.NotSuccessor
 
-        node_parent = node._parent
-        for desc in node._successors:
+        node_parent = node.parent
+        for desc in node.successors:
             desc.connect_to(node_parent)
         del node
 
-    def delete_subtree(self, subroot : ForestNode):
+    def delete_subtree(self, subroot: ForestNode):
         self._validate_nodes(subroot)
 
-        for desc in subroot._successors:
+        for desc in subroot.successors:
             self.delete_subtree(desc)
         if subroot.is_root():
             self._roots.remove(subroot)
@@ -183,16 +204,16 @@ class Forest:
         self._cur_node = self._roots[0]
         return self
 
-    # TODO: Traverse by subroot with syntax indentical to traverse by the whole tree
+    # TODO: Traverse by subroot with syntax identical to traverse by the whole tree
     def __next__(self):
         if self._cur_node.is_parent():
             self._cur_indices.append(0)
-            self._cur_node = self._cur_node._successors[0]
+            self._cur_node = self._cur_node.successors[0]
             return self._cur_node
 
         self._cur_indices[-1] += 1
-        if len(self._cur_node._parent._successors)-1 >= self._cur_indices[-1]:
-            self._cur_node = self._cur_node._parent._successors[self._cur_indices[-1]]
+        if len(self._cur_node.parent.successors)-1 >= self._cur_indices[-1]:
+            self._cur_node = self._cur_node.parent.successors[self._cur_indices[-1]]
             return self._cur_node
 
         while len(self._cur_node.parent.descendants)-1 == self._cur_indices[-1]:
@@ -225,7 +246,7 @@ class Forest:
     def _create_node(self) -> ForestNode:
         return Forest.ForestNode(self)
 
-    def _make_root(self, node : ForestNode):
+    def _make_root(self, node: ForestNode):
         node.disconnect()
         self._roots.append(node)
 
@@ -245,20 +266,20 @@ class Tree(Forest):
         self._roots = (self._create_node(),)
 
     @staticmethod
-    def create(node : Node) -> Tree:
+    def create(node: Node) -> Tree:
         # TODO : Copy successors or not
         pass
 
     def create_root(self):
         raise Tree.AlreadyExistingRoot
 
-    def free_node(self, node : Forest.ForestNode) -> Tree:
+    def free_node(self, node: Forest.ForestNode) -> Tree:
         # TODO: To implement
         pass
 
-    def free_subtree(self, subroot : Forest.ForestNode) -> Tree:
+    def free_subtree(self, subroot: Forest.ForestNode) -> Tree:
         # TODO: To implement
         pass
 
-    def _make_root(self, node : Forest.ForestNode):
+    def _make_root(self, node: Forest.ForestNode):
         raise Tree.AlreadyExistingRoot
--- tree_test.py
+++ tree_test.py
@@ -1,10 +1,72 @@
+
 import unittest
+from tree import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+def build_tree(subtrees, parent=None):
+    root = Node()
+    root.content = subtrees[0]
+    subtrees = subtrees[1:]
 
+    if parent is not None:
+        parent.successors.append(root)
+        root.parent = parent
+
+    for subtree in subtrees:
+        if type(subtree) == list:
+            build_tree(subtree, root)
+        else:
+            successor = Node()
+            successor.content = subtree
+            root.successors.append(successor)
+            successor.parent = root
+
+    return root
+
+
+class NodeFullTreeMatcher:
+    expected: Node
+
+    def __init__(self, expected):
+        self.expected = expected
+
+    def __eq__(self, other: Node):
+        if self.expected.parent != other.parent:
+            return False
+        if self.expected.content != other.content:
+            return False
+
+        for index in range(len(self.expected.successors)):
+            successor_matcher = NodeFullTreeMatcher(self.expected.successors[index])
+            if not successor_matcher.__eq__(other.successors[index]):
+                return False
+
+        return True
+
+class TestNode(unittest.TestCase):
+    def test_disconnect_cases(self):
+        node = build_tree([3])
+        comparing_node = build_tree([3])
+        is_exception = False
+        try:
+            node.disconnect()
+        except:
+            is_exception = True
+        self.assertFalse(is_exception)
+        self.assertEqual(NodeFullTreeMatcher(comparing_node), node)
+        # TODO: Test disconnection from None, from root, from inner node, leaf and subtree
+
+    def test_connect_to(self):
+        pass
+        # TODO: Test connect to root, node above, node below, leaf,
+        #       inner and terminal successors, None, root, inner node and leaf in another Tree
+
+class TestForest(unittest.TestCase):
+    pass
+
+class TestTree(unittest.TestCase):
+    pass
+    # TODO: Test tree-specific content and some forest functions on tree
 
 if __name__ == '__main__':
     unittest.main()
