9a9360f925223987573c17331ce7d328e1adbb4e
Alexander Safonov <safonov@arrival.com>
2023-07-28 14:37:40 +0400

    Disconnection tests pass

--- tree_test.py
+++ tree_test.py
@@ -31,9 +31,9 @@ class NodeFullTreeMatcher:
         self.expected = expected
 
     def __eq__(self, other: Node):
-        if self.expected.parent != other.parent:
-            return False
         if self.expected.content != other.content:
+            return False
+        if len(self.expected.successors) != len(other.successors):
             return False
 
         for index in range(len(self.expected.successors)):
@@ -46,33 +46,60 @@ class NodeFullTreeMatcher:
 
 class TestNodeDisconnect(unittest.TestCase):
     def test_disconnect_leaf_from_none(self):
-        node = build_tree([3])
-        comparing_node = build_tree([3])
-        is_exception = False
-        try:
-            node.disconnect()
-        except:
-            is_exception = True
-        self.assertFalse(is_exception)
-        self.assertEqual(NodeFullTreeMatcher(comparing_node), node)
+        tested_node = build_tree([3])
+        proper_node = build_tree([3])
+        tested_node.disconnect()
+        self.fully_assert(tested_node, tested_node, proper_node, proper_node)
 
     def test_disconnect_subtree_from_none(self):
-        subroot = build_tree([3, 4, [9, 7, 5]])
-        # comparing_subroot = build_tree([3, 4, [9, 7, 5]])
-        # is_exception = False
-        # try:
-        #     subroot.disconnect()
-        # except:
-        #     is_exception = True
-        # self.assertFalse(is_exception)
-        # self.assertEqual(NodeFullTreeMatcher(comparing_subroot), subroot)
+        tested_node = build_tree([3, 4, [9, 7, 5]])
+        proper_node = build_tree([3, 4, [9, 7, 5]])
+        tested_node.disconnect()
+        self.fully_assert(tested_node, tested_node, proper_node, proper_node)
 
-        # root = build_tree([1, [2, 9, [8, 5, 7, 2], 8], 5 , [3, 4, [5, 6, 7], [2, 5, 9]], 7])
-        # node = root.successors[0].successors[0]
-        # TODO: Test disconnection from None, from root, from inner node, leaf and subtree
+    def test_disconnect_leaf_from_root(self):
+        tested_root = build_tree([1, [2, 9, [8, 5, 7, 2], 8], 5, [3, 4, [5, 6, 7], [2, 5, 9]], 7])
+        tested_node = tested_root.successors[1]
+        proper_root = build_tree([1, [2, 9, [8, 5, 7, 2], 8], [3, 4, [5, 6, 7], [2, 5, 9]], 7])
+        proper_node = build_tree([5])
 
-    def test_connect_to(self):
-        pass
+        tested_node.disconnect()
+        self.fully_assert(tested_node, tested_root, proper_node, proper_root)
+
+    def test_disconnect_subtree_from_root(self):
+        tested_root = build_tree([1, [2, 9, [8, 5, 7, 2], 8], 5, [3, 4, [5, 6, 7], [2, 5, 9]], 7])
+        tested_node = tested_root.successors[0]
+        proper_root = build_tree([1, 5, [3, 4, [5, 6, 7], [2, 5, 9]], 7])
+        proper_node = build_tree([2, 9, [8, 5, 7, 2], 8])
+
+        tested_node.disconnect()
+        self.fully_assert(tested_node, tested_root, proper_node, proper_root)
+
+    def test_disconnect_leaf_from_inner_node(self):
+        tested_root = build_tree([1, [2, 9, [8, 5, 7, 2], 8], 5, [3, 4, [5, 6, 7, 1, 8, 0, 4], [2, 5, 9]], 7])
+        tested_node = tested_root.successors[2].successors[1].successors[5]
+        proper_root = build_tree([1, [2, 9, [8, 5, 7, 2], 8], 5, [3, 4, [5, 6, 7, 1, 8, 0], [2, 5, 9]], 7])
+        proper_node = build_tree([4])
+
+        tested_node.disconnect()
+        self.fully_assert(tested_node, tested_root, proper_node, proper_root)
+
+    def test_disconnect_subtree_from_inner_node(self):
+        tested_root = build_tree([1, [2, 9, [8, 5, 7, 2], 8], 5, [3, 4, [5, 6, 7], [2, 5, 9]], 7])
+        tested_node = tested_root.successors[2].successors[2]
+        proper_root = build_tree([1, [2, 9, [8, 5, 7, 2], 8], 5, [3, 4, [5, 6, 7]], 7])
+        proper_node = build_tree([2, 5, 9])
+
+        tested_node.disconnect()
+        self.fully_assert(tested_node, tested_root, proper_node, proper_root)
+
+    def fully_assert(self, tested_node: Node, tested_root: Node, proper_node: Node, proper_root: Node):
+        self.assertEqual(None, tested_node.parent)
+        self.assertEqual(NodeFullTreeMatcher(proper_node), tested_node)
+        self.assertEqual(NodeFullTreeMatcher(proper_root), tested_root)
+
+    # def test_connect_to(self):
+    #     pass
         # TODO: Test connect to root, node above, node below, leaf,
         #       inner and terminal successors, None, root, inner node and leaf in another Tree
 
