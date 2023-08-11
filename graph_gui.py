064ad20f656207a30eb03340088904ce1cbb724c
Alexander Safonov <safonov@arrival.com>
2023-08-11 18:03:38 +0400

    Childer are being added, but without realignment (they collide).

--- graph_gui.py
+++ graph_gui.py
@@ -10,6 +10,12 @@ from PyQt6.QtCore import *
 
 # TODO: Try to separate scene from view creating another class GraphGUI referencing both them
 class GraphView(QGraphicsView):
+    HORIZONTAL_GAP = 50
+    VERTICAL_GAP = 50
+
+    CONNECTION_LINE_COLOR = QColorConstants.Red
+    CONNECTION_LINE_THICKNESS = 3
+
     class ExtraDrawingArea(Exception): pass
 
     def __init__(self, parent: QWidget=None):
@@ -26,19 +32,43 @@ class GraphView(QGraphicsView):
 
     def add_root(self):
         rect = GraphNode()
-        rect.new_child_called.connect(self.add_child)
+        rect.setData(GraphView._ITEM_DATA_KEYS_DICT["children_number"], 0)
+        rect.newChildCalled.connect(self.add_child)
         self._scene.addItem(rect)
         x, y = self._calc_new_root_coords()
         rect.moveBy(x, y)
 
-    def add_child(self):
-        sys.exit()
+    @pyqtSlot(QGraphicsItem)
+    def add_child(self, parent: QGraphicsItem):
+        child = GraphNode()
+        child.setData(GraphView._ITEM_DATA_KEYS_DICT["children_number"], 0)
+        self._scene.addItem(child)
+        parent_children_number = parent.data(GraphView._ITEM_DATA_KEYS_DICT["children_number"])
+        x = parent.pos().x() + GraphNode.WIDTH + GraphView.HORIZONTAL_GAP
+        y = parent.pos().y() + (GraphNode.HEIGHT + GraphView.VERTICAL_GAP) * parent_children_number
+        child.moveBy(x, y)
+        parent.setData(GraphView._ITEM_DATA_KEYS_DICT["children_number"], parent_children_number+1)
+        child.newChildCalled.connect(self.add_child)
+
+        parent_x = parent.pos().x() + GraphNode.WIDTH
+        parent_y = parent.pos().y() + GraphNode.HEIGHT/2
+        child_x = x
+        child_y = y + GraphNode.HEIGHT/2
+        connection_line = QGraphicsLineItem(parent_x, parent_y, child_x, child_y)
+        pen = QPen(GraphView.CONNECTION_LINE_COLOR, GraphView.CONNECTION_LINE_THICKNESS)
+        connection_line.setPen(pen)
+        self._scene.addItem(connection_line)
+
 
     def _calc_new_root_coords(self):
         x = GraphView._X
         y = self._root_insertion_y
         self._root_insertion_y += 2 * GraphNode.HEIGHT
         return x, y
+
+    _ITEM_DATA_KEYS_DICT = {
+        "children_number" : 0
+    }
 
     _X = 50
 
@@ -64,7 +94,7 @@ class GraphNode(QGraphicsObject):
     def __init__(self):
         super().__init__(None)
         cross = CrossIcon(self)
-        cross.clicked.connect(self.new_child_called)
+        cross.clicked.connect(self._receiveNewChildClick)
         cross.moveBy(GraphNode.WIDTH+GraphNode.CROSS_GAP, 0)
 
     def boundingRect(self) -> QRectF:
@@ -82,7 +112,11 @@ class GraphNode(QGraphicsObject):
         painter.setBrush(brush)
         painter.drawRoundedRect(0, 0, GraphNode.WIDTH, GraphNode.HEIGHT, GraphNode.ROUNDING, GraphNode.ROUNDING)
 
-    new_child_called = pyqtSignal()
+    @pyqtSlot()
+    def _receiveNewChildClick(self):
+        self.newChildCalled.emit(self)
+
+    newChildCalled = pyqtSignal(QGraphicsItem)
 
 
 class CrossIcon(QGraphicsWidget):
